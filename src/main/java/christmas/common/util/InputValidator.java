9f796b313455fb5dbb3f2701e0f5186f0e038dd6
sunwoong <sunwoong1122@gmail.com>
2023-11-15 15:33:56 +0900

    feat: 사용자 입력 값 검증 기능 추가

--- src/main/java/christmas/common/util/InputValidator.java
+++ src/main/java/christmas/common/util/InputValidator.java
@@ -2,9 +2,7 @@ package christmas.common.util;
 
 import christmas.domain.Menu;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public interface InputValidator {
     static void validateVisitDate(String input) {
@@ -19,12 +17,15 @@ public interface InputValidator {
     static void validateMenu(String input) {
         validateIsBlank(input);
         if (validateIsEdgeCase(input)) {
+            validateIsValidMenuEdgeCase(input);
+            validateIsValidEdgeCaseTotalCount(input);
             return;
         }
         validateIsContainSymbol(input);
         validateIsValidPosition(input);
         validateIsValidPattern(input);
         validateIsNotDuplicateAndValidMenu(input);
+        validateIsValidTotalCount(input);
     }
 
     private static void validateIsValidRange(int processedInput) {
@@ -40,7 +41,20 @@ public interface InputValidator {
     }
 
     private static boolean validateIsEdgeCase(String input) {
-        return input.matches("^[가-힣]+-[1-9]+$");
+        return input.matches("^[가-힣]+-[0-9]+$");
+    }
+
+    private static void validateIsValidMenuEdgeCase(String input) {
+        String[] processedData = input.split("-");
+        Menu.of(processedData[0]);
+    }
+
+    private static void validateIsValidEdgeCaseTotalCount(String input) {
+        String[] processedData = input.split("-");
+        int totalCount = Integer.parseInt(processedData[1]);
+        if (totalCount > 20 || totalCount < 1) {
+            throw new IllegalArgumentException();
+        }
     }
 
     private static void validateIsContainSymbol(String input) {
@@ -58,7 +72,7 @@ public interface InputValidator {
     private static void validateIsValidPattern(String input) {
         String[] processedInput = input.split(",");
         boolean isMismatch = Arrays.stream(processedInput)
-                .anyMatch(data -> !data.matches("^[가-힣]+-[1-9]+$"));
+                .anyMatch(data -> !data.matches("^[가-힣]+-[0-9]+$"));
         if (isMismatch) {
             throw new IllegalArgumentException();
         }
@@ -67,14 +81,51 @@ public interface InputValidator {
     private static void validateIsNotDuplicateAndValidMenu(String input) {
         String[] processedInput = input.split(",");
         List<Menu> seen = new ArrayList<>();
+        Set<String> kind = new HashSet<>();
         Arrays.stream(processedInput)
                 .forEach(data -> {
                     String[] processedData = data.split("-");
-                    Menu menu = Menu.of(processedData[0]);
-                    if (seen.contains(menu)) {
-                        throw new IllegalArgumentException();
-                    }
+                    Menu menu = validateIsValidMenuAndGetMenu(processedData);
+                    validateIsValidCount(processedData);
+                    validateDuplicateMenu(seen, menu);
+                    kind.add(menu.getKind());
                     seen.add(menu);
                 });
+        validateOnlyDrinkMenu(kind);
+    }
+
+    private static Menu validateIsValidMenuAndGetMenu(String[] processedData) {
+        return Menu.of(processedData[0]);
+    }
+
+    private static void validateIsValidCount(String[] processedData) {
+        if (Integer.parseInt(processedData[1]) < 1) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static void validateDuplicateMenu(List<Menu> seen, Menu menu) {
+        if (seen.contains(menu)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static void validateOnlyDrinkMenu(Set<String> kind) {
+        if (kind.contains("drink") && kind.size() == 1) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static void validateIsValidTotalCount(String input) {
+        String[] processedInput = input.split(",");
+        int totalCount = Arrays.stream(processedInput)
+                .mapToInt(data -> {
+                    String[] processedData = data.split("-");
+                    return Integer.parseInt(processedData[1]);
+                })
+                .sum();
+        if (totalCount > 20) {
+            throw new IllegalArgumentException();
+        }
     }
 }
