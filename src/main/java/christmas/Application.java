b7111ad491f68d11e81c7e7381e193dda93b1f8b
sunwoong <sunwoong1122@gmail.com>
2023-11-15 17:27:11 +0900

    feat: 이벤트 플래너 시작 기능 구현

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,12 @@
 package christmas;
 
+import christmas.common.config.DependencyContainer;
+import christmas.controller.ChristmasController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        DependencyContainer dependencyContainer = new DependencyContainer();
+        ChristmasController christmasController = dependencyContainer.getChristmasController();
+        christmasController.startEventPlanner();
     }
 }
