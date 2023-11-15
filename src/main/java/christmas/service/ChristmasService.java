6004ccbf991893605b20f3d52cc06ef5cdd476be
sunwoong <sunwoong1122@gmail.com>
2023-11-15 17:55:58 +0900

    refactor: 서비스 로직 메서드 분리

--- src/main/java/christmas/service/ChristmasService.java
+++ src/main/java/christmas/service/ChristmasService.java
@@ -1,27 +1,35 @@
 package christmas.service;
 
-import christmas.domain.Badge;
 import christmas.domain.Event;
 import christmas.domain.Order;
 import christmas.dto.Result;
 
 public class ChristmasService {
     public Result applyEvent(int visitDate, Order order) {
-        Event event = new Event();
-        boolean hasEvent = event.hasEvent(order);
+        Event event = generateEvent();
+        boolean hasEvent = hasEvent(order, event);
         if (hasEvent) {
-            boolean hasGiftEvent = event.hasGiftEvent(order);
-            int christmasDiscount = event.calculateChristmasDiscount(visitDate);
-            int weekdayDiscount = event.calculateWeekdayDiscount(visitDate, order);
-            int weekendDiscount = event.calculateWeekendDiscount(visitDate, order);
-            int specialDiscount = event.calculateSpecialDiscount(visitDate);
-            int discountPrice = event.getDiscountPrice();
-            Badge eventBadge = event.getEventBadge(order);
-            int profitPrice = event.calculateProfitPrice(order);
-            return new Result(true, hasGiftEvent, christmasDiscount, weekdayDiscount, weekendDiscount,
-                    specialDiscount, profitPrice, order.calculateTotalOrderAmount() - discountPrice, eventBadge);
+            return calculateEventAndGetResult(visitDate, order, event);
         }
-        return new Result(false, false, 0, 0, 0,
+        return getDefaultResult(order, event);
+    }
+
+    private Event generateEvent() {
+        return new Event();
+    }
+
+    private boolean hasEvent(Order order, Event event) {
+        return event.hasEvent(order);
+    }
+
+    private Result calculateEventAndGetResult(int visitDate, Order order, Event event) {
+        return Result.of(true, event.hasGiftEvent(order), event.calculateChristmasDiscount(visitDate), event.calculateWeekdayDiscount(visitDate, order),
+                event.calculateWeekendDiscount(visitDate, order), event.calculateSpecialDiscount(visitDate), event.calculateProfitPrice(order),
+                order.calculateTotalOrderAmount() - event.getDiscountPrice(), event.getEventBadge(order));
+    }
+
+    private Result getDefaultResult(Order order, Event event) {
+        return Result.of(false, false, 0, 0, 0,
                 0, 0, order.calculateTotalOrderAmount(), event.getEventBadge(order));
     }
 }
