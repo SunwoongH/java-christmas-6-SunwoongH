62fc945d8fbc8ca8f12da602cbd5dccd0a3f5e7b
sunwoong <sunwoong1122@gmail.com>
2023-11-15 19:18:32 +0900

    refactor: 사용자 입력 값 검증기 크리스마스 예외 & enum 적용

--- src/main/java/christmas/common/util/InputValidator.java
+++ src/main/java/christmas/common/util/InputValidator.java
@@ -1,5 +1,7 @@
 package christmas.common.util;
 
+import christmas.common.constant.ErrorMessage;
+import christmas.common.error.ChristmasException;
 import christmas.domain.Menu;
 
 import java.util.*;
@@ -10,7 +12,7 @@ public interface InputValidator {
             int processedInput = Integer.parseInt(input);
             validateIsValidRange(processedInput);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException();
+            throw new ChristmasException(ErrorMessage.ERROR_VISIT_DATE);
         }
     }
 
@@ -30,13 +32,13 @@ public interface InputValidator {
 
     private static void validateIsValidRange(int processedInput) {
         if (processedInput < 1 || processedInput > 31) {
-            throw new IllegalArgumentException();
+            throw new ChristmasException(ErrorMessage.ERROR_VISIT_DATE);
         }
     }
 
     private static void validateIsBlank(String input) {
         if (input.isBlank()) {
-            throw new IllegalArgumentException();
+            throw new ChristmasException(ErrorMessage.ERROR_ORDER);
         }
     }
 
@@ -53,19 +55,19 @@ public interface InputValidator {
         String[] processedData = input.split("-");
         int totalCount = Integer.parseInt(processedData[1]);
         if (totalCount > 20 || totalCount < 1) {
-            throw new IllegalArgumentException();
+            throw new ChristmasException(ErrorMessage.ERROR_ORDER);
         }
     }
 
     private static void validateIsContainSymbol(String input) {
         if (!input.contains(",")) {
-            throw new IllegalArgumentException();
+            throw new ChristmasException(ErrorMessage.ERROR_ORDER);
         }
     }
 
     private static void validateIsValidPosition(String input) {
         if (input.startsWith(",") || input.endsWith(",")) {
-            throw new IllegalArgumentException();
+            throw new ChristmasException(ErrorMessage.ERROR_ORDER);
         }
     }
 
@@ -74,7 +76,7 @@ public interface InputValidator {
         boolean isMismatch = Arrays.stream(processedInput)
                 .anyMatch(data -> !data.matches("^[가-힣]+-[0-9]+$"));
         if (isMismatch) {
-            throw new IllegalArgumentException();
+            throw new ChristmasException(ErrorMessage.ERROR_ORDER);
         }
     }
 
@@ -100,19 +102,19 @@ public interface InputValidator {
 
     private static void validateIsValidCount(String[] processedData) {
         if (Integer.parseInt(processedData[1]) < 1) {
-            throw new IllegalArgumentException();
+            throw new ChristmasException(ErrorMessage.ERROR_ORDER);
         }
     }
 
     private static void validateDuplicateMenu(List<Menu> seen, Menu menu) {
         if (seen.contains(menu)) {
-            throw new IllegalArgumentException();
+            throw new ChristmasException(ErrorMessage.ERROR_ORDER);
         }
     }
 
     private static void validateOnlyDrinkMenu(Set<String> kind) {
         if (kind.contains("drink") && kind.size() == 1) {
-            throw new IllegalArgumentException();
+            throw new ChristmasException(ErrorMessage.ERROR_ORDER);
         }
     }
 
@@ -125,7 +127,7 @@ public interface InputValidator {
                 })
                 .sum();
         if (totalCount > 20) {
-            throw new IllegalArgumentException();
+            throw new ChristmasException(ErrorMessage.ERROR_ORDER);
         }
     }
 }
