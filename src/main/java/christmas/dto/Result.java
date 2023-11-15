3e5c1401edbf49ce7b4f63fac69f0a7839ddfe73
sunwoong <sunwoong1122@gmail.com>
2023-11-15 17:55:46 +0900

    feat: 정적 메서드 객체 생성 기능 구현

--- src/main/java/christmas/dto/Result.java
+++ src/main/java/christmas/dto/Result.java
@@ -13,4 +13,9 @@ public record Result(
         int paymentPrice,
         Badge eventBadge
 ) {
+    public static Result of(boolean hasEvent, boolean hasGiftEvent, int christmasDiscount, int weekdayDiscount,
+                            int weekendDiscount, int specialDiscount, int profitPrice, int paymentPrice, Badge eventBadge) {
+        return new Result(hasEvent, hasGiftEvent, christmasDiscount, weekdayDiscount, weekendDiscount,
+                specialDiscount, profitPrice, paymentPrice, eventBadge);
+    }
 }
