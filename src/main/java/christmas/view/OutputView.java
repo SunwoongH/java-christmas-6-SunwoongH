8d4f006d4601daafbc3b2a31f960dd6cb893454d
sunwoong <sunwoong1122@gmail.com>
2023-11-15 18:49:52 +0900

    refactor: 출력 문자열 enum 변경 & 메서드 분리

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,5 +1,6 @@
 package christmas.view;
 
+import christmas.common.constant.PrintMessage;
 import christmas.domain.Menu;
 import christmas.domain.Order;
 import christmas.dto.Result;
@@ -9,71 +10,167 @@ import java.util.Map;
 
 public class OutputView {
     public void printVisitDateEvent(int visitDate) {
-        System.out.printf("12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n", visitDate);
+        System.out.printf(String.valueOf(PrintMessage.OUTPUT_VISIT_DATE_EVENT), visitDate);
     }
 
     public void printMenu(Order order) {
-        System.out.println("\n<주문 메뉴>");
-        Map<Menu, Integer> menus = order.getMenus();
-        menus.forEach((key, value) -> System.out.printf("%s %d개\n", key.getName(), value));
+        printMenuTitle();
+        printMenus(order);
     }
 
     public void printTotalOrderAmount(Order order) {
-        System.out.println("\n<할인 전 총주문 금액>");
-        DecimalFormat decimalFormat = new DecimalFormat("###,###");
-        System.out.printf("%s원\n", decimalFormat.format(order.calculateTotalOrderAmount()));
+        printTotalOrderAmountTitle();
+        DecimalFormat decimalFormat = generatePriceDecimalFormat();
+        printTotalOrderAmountContent(order, decimalFormat);
     }
 
     public void printGiftEvent(Result result) {
-        System.out.println("\n<증정 메뉴>");
+        printGiftEventTitle();
         if (result.hasGiftEvent()) {
-            System.out.println("샴페인 1개");
+            printGiftEventIfApply();
             return;
         }
-        System.out.println("없음");
+        printDefault();
     }
 
     public void printEvent(Result result) {
-        System.out.println("\n<혜택 내역>");
-        if (!result.hasEvent()) {
-            System.out.println("없음");
-        }
-        DecimalFormat decimalFormat = new DecimalFormat("###,###");
-        if (result.christmasDiscount() > 0) {
-            System.out.printf("크리스마스 디데이 할인: -%s원\n", decimalFormat.format(result.christmasDiscount()));
-        }
-        if (result.weekdayDiscount() > 0) {
-            System.out.printf("평일 할인: -%s원\n", decimalFormat.format(result.weekdayDiscount()));
-        }
-        if (result.weekendDiscount() > 0) {
-            System.out.printf("주말 할인: -%s원\n", decimalFormat.format(result.weekendDiscount()));
-        }
-        if (result.specialDiscount() > 0) {
-            System.out.printf("특별 할인: -%s원\n", decimalFormat.format(result.specialDiscount()));
-        }
-        if (result.hasGiftEvent()) {
-            System.out.println("증정 이벤트: -25,000원");
-        }
+        printEventTitle();
+        printDefaultIfNotApplyEvent(result);
+        DecimalFormat decimalFormat = generatePriceDecimalFormat();
+        printChristmasEventIfApply(result, decimalFormat);
+        printWeekdayEventIfApply(result, decimalFormat);
+        printWeekendEventIfApply(result, decimalFormat);
+        printSpecialEventIfApply(result, decimalFormat);
+        printGiftEventIfApply(result);
     }
 
     public void printTotalProfitPrice(Result result) {
-        System.out.println("\n<총혜택 금액>");
-        DecimalFormat decimalFormat = new DecimalFormat("###,###");
+        printTotalProfitPriceTitle();
+        DecimalFormat decimalFormat = generatePriceDecimalFormat();
         if (result.profitPrice() == 0) {
-            System.out.println("0원");
+            printTotalProfitPriceIfNotApply();
             return;
         }
-        System.out.printf("-%s원\n", decimalFormat.format(result.profitPrice()));
+        printTotalProfitPriceContent(result, decimalFormat);
     }
 
     public void printPaymentPrice(Result result) {
-        System.out.println("\n<할인 후 예상 결제 금액>");
-        DecimalFormat decimalFormat = new DecimalFormat("###,###");
-        System.out.printf("%s원\n", decimalFormat.format(result.paymentPrice()));
+        printPaymentPriceTitle();
+        DecimalFormat decimalFormat = generatePriceDecimalFormat();
+        printPaymentPriceContent(result, decimalFormat);
     }
 
     public void printEventBadge(Result result) {
-        System.out.println("\n<12월 이벤트 배지>");
+        printEventBadgeTitle();
+        printEventBadgeContent(result);
+    }
+
+    private void printMenuTitle() {
+        System.out.println(PrintMessage.OUTPUT_MENU_TITLE);
+    }
+
+    private void printMenus(Order order) {
+        Map<Menu, Integer> menus = order.getMenus();
+        menus.forEach((key, value) ->
+                System.out.printf(String.valueOf(PrintMessage.OUTPUT_MENU), key.getName(), value));
+    }
+
+    private void printTotalOrderAmountTitle() {
+        System.out.println(PrintMessage.OUTPUT_TOTAL_ORDER_AMOUNT_TITLE);
+    }
+
+    private void printTotalOrderAmountContent(Order order, DecimalFormat decimalFormat) {
+        System.out.printf(String.valueOf(PrintMessage.OUTPUT_TOTAL_ORDER_AMOUNT),
+                decimalFormat.format(order.calculateTotalOrderAmount()));
+    }
+
+    private void printGiftEventTitle() {
+        System.out.println(PrintMessage.OUTPUT_GIFT_EVENT_TITLE);
+    }
+
+    private void printGiftEventIfApply() {
+        System.out.println(PrintMessage.OUTPUT_GIFT_EVENT);
+    }
+
+    private void printEventTitle() {
+        System.out.println(PrintMessage.OUTPUT_EVENT_TITLE);
+    }
+
+    private void printDefaultIfNotApplyEvent(Result result) {
+        if (!result.hasEvent()) {
+            printDefault();
+        }
+    }
+
+    private void printChristmasEventIfApply(Result result, DecimalFormat decimalFormat) {
+        if (result.christmasDiscount() > 0) {
+            System.out.printf(String.valueOf(PrintMessage.OUTPUT_CHRISTMAS_EVENT),
+                    decimalFormat.format(result.christmasDiscount()));
+        }
+    }
+
+    private void printWeekdayEventIfApply(Result result, DecimalFormat decimalFormat) {
+        if (result.weekdayDiscount() > 0) {
+            System.out.printf(String.valueOf(PrintMessage.OUTPUT_WEEKDAY_EVENT),
+                    decimalFormat.format(result.weekdayDiscount()));
+        }
+    }
+
+    private void printWeekendEventIfApply(Result result, DecimalFormat decimalFormat) {
+        if (result.weekendDiscount() > 0) {
+            System.out.printf(String.valueOf(PrintMessage.OUTPUT_WEEKEND_EVENT),
+                    decimalFormat.format(result.weekendDiscount()));
+        }
+    }
+
+    private void printSpecialEventIfApply(Result result, DecimalFormat decimalFormat) {
+        if (result.specialDiscount() > 0) {
+            System.out.printf(String.valueOf(PrintMessage.OUTPUT_SPECIAL_EVENT),
+                    decimalFormat.format(result.specialDiscount()));
+        }
+    }
+
+    private void printGiftEventIfApply(Result result) {
+        if (result.hasGiftEvent()) {
+            System.out.println(PrintMessage.OUTPUT_GIFT_PRICE_EVENT);
+        }
+    }
+
+    private void printTotalProfitPriceTitle() {
+        System.out.println(PrintMessage.OUTPUT_TOTAL_PROFIT_PRICE_TITLE);
+    }
+
+    private void printTotalProfitPriceIfNotApply() {
+        System.out.println(PrintMessage.OUTPUT_DEFAULT_TOTAL_PROFIT_PRICE);
+    }
+
+    private void printTotalProfitPriceContent(Result result, DecimalFormat decimalFormat) {
+        System.out.printf(String.valueOf(PrintMessage.OUTPUT_TOTAL_PROFIT_PRICE),
+                decimalFormat.format(result.profitPrice()));
+    }
+
+    private void printPaymentPriceTitle() {
+        System.out.println(PrintMessage.OUTPUT_PAYMENT_PRICE_TITLE);
+    }
+
+    private void printPaymentPriceContent(Result result, DecimalFormat decimalFormat) {
+        System.out.printf(String.valueOf(PrintMessage.OUTPUT_PAYMENT_PRICE),
+                decimalFormat.format(result.paymentPrice()));
+    }
+
+    private void printEventBadgeTitle() {
+        System.out.println(PrintMessage.OUTPUT_EVENT_BADGE_TITLE);
+    }
+
+    private void printEventBadgeContent(Result result) {
         System.out.println(result.eventBadge().getName());
+    }
+
+    private DecimalFormat generatePriceDecimalFormat() {
+        return new DecimalFormat("###,###");
+    }
+
+    private void printDefault() {
+        System.out.println(PrintMessage.OUTPUT_DEFAULT);
     }
 }
